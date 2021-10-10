7249b99636d3f778bb5460fcaf8483cb2f864067
shanashapiro <89433900+shanashapiro@users.noreply.github.com>
2021-10-10 11:03:01 -0400

    Create ImportARGOS.py

---
+++ ImportARGOS.py
@@ -0,0 +1,11 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2021
+## Author: shana.shapiro@duke.edu (For ENV859) 
+##---------------------------------------------------------------------
