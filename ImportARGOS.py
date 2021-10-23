969583399a316c40d72035a7abef99ee2085ee34
shanashapiro <89433900+shanashapiro@users.noreply.github.com>
2021-10-23 19:18:25 -0400

    Successfully create a point object

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -65,7 +65,30 @@ while lineString:
         obsLC   = lineData[7]
         
         # Print results to see how we're doing
-        print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
+        #print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
+        
+        #Try to convert coordinates to a point object 
+        try: 
+        
+            # Convert raw coordinate strings to numbers
+            if obsLat[-1] == 'N':
+                obsLat = float(obsLat[:-1])
+            else:
+                obsLat = float(obsLat[:-1]) * -1
+            if obsLon[-1] == 'E':
+                obsLon = float(obsLon[:-1])
+            else:
+                obsLon = float(obsLon[:-1]) * -1
+            
+            # Create point object from Lat/Lon coordinates
+            obsPoint = arcpy.Point()
+            obsPoint.X = obsLon
+            obsPoint.Y = obsLat
+            
+        #Handle any error
+        except Exception as e:
+            print(f"Error adding record {tagID} to the output: {e}")
+            
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
